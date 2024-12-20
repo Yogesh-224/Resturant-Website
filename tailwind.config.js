3bdb3cb0941bd267240823db22a8c85c911d12f7
Yogesh-224 <yogeshpandey224@gmail.com>
2024-12-20 16:23:08 +0530

    table reservation section with backend

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,18 @@ module.exports = {
       },height: {
         '36': '9rem',  // 9rem = 144px
       },
-     
+      animation: {
+        colorChange: 'colorChange 3s ease-in-out infinite',
+    },
+    keyframes: {
+        colorChange: {
+            '0%': { backgroundColor: '#b02004' }, /* red-500 */
+            '50%': { backgroundColor: '#874f41' }, /* yellow-500 */
+            '100%': { backgroundColor: '#6e2716' }, /* red-500 */
+        },
+    },
     },
   },
   plugins: [],
-}
+} 
 
